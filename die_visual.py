9cf29f8734e5840f4e7324d42545c7ac1e9a141c
Jace432 <jwebb54@student.cscc.edu>
2025-04-26 16:15:23 -0400

    Altered die_visual to deal with two dice and show the bar graph accordingly.

--- die_visual.py
+++ die_visual.py
@@ -1,23 +1,26 @@
 import plotly.express as px
 from die import Die
 
-die = Die()
+die1 = Die()
+die2 = Die()
 
 results = []
 
 for roll_num in range(100):
-    result = die.roll()
+    result = die1.roll() + die2.roll()
     results.append(result)
 
 frequencies = []
-
-pos_results = range(1, die.num_sides + 1)
+min_result_value = 1 + 1
+max_result_value = die1.num_sides + die2.num_sides
+pos_results = range(min_result_value, max_result_value + 1)
 
 for value in pos_results:
     freq = results.count(value)
     frequencies.append(freq)
 
 fig = px.bar(x=pos_results, y=frequencies, 
-             title=f"Frequence of {die.num_sides} sides of die rolls",
+             title=f"Frequence of rolling two D{die1.num_sides} dice",
              labels={"x" : "Possible Values","y" : "Frequency"})
+fig.update_layout()
 fig.show()
